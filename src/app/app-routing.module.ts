000dd4860f5dd3381ab5980df3ae32ecb577410f
adambialek86 <adamjanbialek@gmail.com>
2023-07-30 19:36:56 +0200

    routing configuration for app module. included lazy loading for feature-top-stories module and preloading strategy. also added root directory consisting of navbar and home components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
+import {PreloadAllModules, RouterModule, Routes} from "@angular/router";
+import {HomeComponent} from "./root/components/home/home.component";
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'top-stories', loadChildren: () => import('./feature-top-stories/top-stories.feature.module')
+      .then(m => m.TopStoriesFeatureModule)
+  },
+]
 
 @NgModule({
-  declarations: [
-  ],
-  imports: [
-  ],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { NavbarComponent } from './root/components/navbar/navbar.component';
+import { HomeComponent } from './root/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
