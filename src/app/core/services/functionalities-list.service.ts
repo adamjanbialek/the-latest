088fd53e05ebb457f7075fbdacc1614cf797cab3
adambialek86 <adamjanbialek@gmail.com>
2023-08-01 00:31:22 +0200

    added list of functionalities and graphical assets connected to them

---
+++ src/app/core/services/functionalities-list.service.ts
@@ -0,0 +1,38 @@
+import {Injectable} from "@angular/core";
+
+export interface Functionality {
+  name: string;
+  url? : string;
+  imageUrl: string;
+  isReady: boolean;
+}
+
+@Injectable({
+  providedIn: "root"
+})
+export class FunctionalitiesListService {
+  functionalitiesArray: Functionality[] = [
+    {
+      name: 'Top Stories',
+      url: 'top-stories',
+      imageUrl: 'assets/news.jpg',
+      isReady: true
+    },
+    {
+      name: 'Forecast',
+      imageUrl: 'assets/weather.jpg',
+      isReady: false
+    },
+    {
+      name: 'Stocks Info',
+      imageUrl: 'assets/stocks.jpg',
+      isReady: false
+    },
+    {
+      name: 'Live Results',
+      imageUrl: 'assets/sports.jpg',
+      isReady: false
+    }
+  ]
+
+}
