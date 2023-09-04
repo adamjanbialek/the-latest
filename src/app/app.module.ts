9ff6d3788990db6512ba386b7e9477717c55799f
adambialek86 <adamjanbialek@gmail.com>
2023-09-04 02:32:23 +0200

    implementation of angular material and recreated navbar using angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import {AppRoutingModule} from "./app-routing.module";
 import { NavbarComponent } from './root/components/navbar/navbar.component';
 import { HomeComponent } from './root/components/home/home.component';
 import {HttpClientModule} from "@angular/common/http";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
 
 @NgModule({
   declarations: [
@@ -16,7 +19,10 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
